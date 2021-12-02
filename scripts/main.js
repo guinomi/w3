c1b27ebe0410a0659230597bb8700c464144ebc7
guinomi <87597387+guinomi@users.noreply.github.com>
2021-12-02 11:26:33 +0900

    Added

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 // Image switcher code
 
 let myImage = document.querySelector('img');
+// documentは組み込みのオブジェクト、
+// ブラウザが表示しているページ、DOM　とも呼ぶ。　Document Object Modelの略。
+// querySelectorはページの一部（HTML要素）を選ぶ関数
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
@@ -18,19 +21,22 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  // promptは組み込みの関数、prompt (pop-up)Windowを表示して、ユーザ入力をさせる。
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを、特別な場所（変数ではない、ファイルなどに相当）に保存する。
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
 if(!localStorage.getItem('name')) {
+  //以前に特罰な場所（ファイルなど）保存していた名前があれば、それを使う。
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'さようなら, ' + storedName;
 }
 
 myButton.onclick = function() {
